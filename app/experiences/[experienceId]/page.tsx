b32c0cd3860c20bf6d379af3db4bd059aac423b0
xyx <2585548392@qq.com>
2025-10-31 21:35:53 +0800

    Render experience UI after access check

--- app/experiences/[experienceId]/page.tsx
+++ app/experiences/[experienceId]/page.tsx
@@ -1,6 +1,6 @@
+import MindfulBreath from "@/components/MindfulBreath";
 import { AccessContainer } from "@/components/access/AccessContainer";
 import { AccessError } from "@/components/access/AccessError";
-import { AccessSummary } from "@/components/access/AccessSummary";
 import { resolveAccess } from "@/lib/whop-access";
 
 type ExperiencePageParams = {
@@ -13,13 +13,24 @@ export default async function ExperiencePage({
 	const { experienceId } = await params;
 	const result = await resolveAccess("experience", experienceId);
 
-	return (
-		<AccessContainer>
-			{result.ok ? (
-				<AccessSummary {...result.data} />
-			) : (
+	if (!result.ok) {
+		return (
+			<AccessContainer>
 				<AccessError title={result.title} message={result.message} />
-			)}
-		</AccessContainer>
-	);
+			</AccessContainer>
+		);
+	}
+
+	if (!result.data.hasAccess) {
+		return (
+			<AccessContainer>
+				<AccessError
+					title="Access required"
+					message="You need active experience access to view this page. Please contact support if you believe this is a mistake."
+				/>
+			</AccessContainer>
+		);
+	}
+
+	return <MindfulBreath />;
 }
